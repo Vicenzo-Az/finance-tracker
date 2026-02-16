1ea9a827ee446ad84ea45af81aee15c357b594f4
Vicenzo-Az <vicenzo.net@gmail.com>
2026-02-16 03:58:48 -0300

    feat: integrate react-router-dom for routing and update App component structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import { AppLayout } from "./components/layout/AppLayout";
 import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import Transactions from "./pages/Transactions";
 
 function App() {
   return (
     <AppLayout>
-      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
     </AppLayout>
   );
 }
